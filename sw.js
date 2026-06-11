c7a6957781433bb40412ce04cc7884ab30fe5150
ihrealltagsbegleiter <ihrealltagsbegleiter@gmail.com>
2026-06-12 00:56:47 +0200

    Unsichtbare Arbeit: Schon-erledigt-Log, Angefangen-Schleifen, Ungeplant-Bilanz + Protokoll-Loeschung (sw.js) – 77/77 Tests

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Aufgaben PWA Service Worker v2 – Network-First
 // Scope: /aufgaben/  |  Keine Abhängigkeit zu anderen App-Repos
-const CACHE = 'iab-aufgaben-v11';
+const CACHE = 'iab-aufgaben-v12';
 const APP_SHELL = [
   '/aufgaben/',
   '/aufgaben/index.html',

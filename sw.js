6205ba9d0186ff9d9220077f4ded8c17eaa39917
ihrealltagsbegleiter <ihrealltagsbegleiter@gmail.com>
2026-06-12 01:11:47 +0200

    Sync-Fix Log-Loeschung (Tombstones), Mehr-Menue, Tagesbericht 18 Uhr (sw.js) – 91/91 Tests

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Aufgaben PWA Service Worker v2 – Network-First
 // Scope: /aufgaben/  |  Keine Abhängigkeit zu anderen App-Repos
-const CACHE = 'iab-aufgaben-v13';
+const CACHE = 'iab-aufgaben-v14';
 const APP_SHELL = [
   '/aufgaben/',
   '/aufgaben/index.html',
